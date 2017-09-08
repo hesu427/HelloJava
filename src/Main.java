b44618e27d72bdb9f7e952e2bc24aa909f8ae90f
heesu <hesu427@naver.com>
2017-09-08 12:55:47 +0900

    Revert "Version3 복제된 버전 작업"
    This reverts commit 8610f1d934141b3403731be6917b4bc4c50668ae.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,2 @@
-public class Main {
-    public static void main(String[] args){
-
         System.out.println("Hello Java");
-
         System.out.println("New version");
-
-        System.out.println("Version3 복제된 버전 작업");
-    }
-
-}
-
