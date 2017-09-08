c91ae188a993a66aa0e758fdbbd7a097390150de
heesu <hesu427@naver.com>
2017-09-08 12:57:02 +0900

    Version 3 복제된버전

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
         System.out.println("Hello Java");
+
         System.out.println("New version");
+
+        System.out.println("Version 3 복제된버전");
+    }
+
+}
+
